a3807ef454b8a165d02d0b6b4224f72186e6fde1
mdg0410 <levoyersteven@gmail.com>
2023-03-05 22:56:01 -0500

    B

--- modules/bookForm.js
+++ modules/bookForm.js
@@ -1,6 +1,23 @@
-export default class bookForm {
-  constructor(form, index = null) {
-    this.form = document.forms[form];
-    this.index = index;
+import Book from './Book.js';
+import { library } from './Library.js';
+
+export class BookForm {
+  constructor() {
+    this.form = document.forms.bookForm;
+    this.form.onsubmit = this.submit;
+  }
+
+  submit = (e) => {
+    e.preventDefault();
+    const title = this.form.title.value;
+    const author = this.form.author.value;
+    const newBook = new Book(title, author);
+
+    library.addBook(newBook);
+
+    this.form.title.focus();
+    this.form.reset();
   }
 }
+
+export default new BookForm();
