2b2159f4300002bbd3c7dcfe8086e9d6284cca79
mdg0410 <levoyersteven@gmail.com>
2023-03-06 12:49:43 -0500

    Fix & Solved error

--- modules/bookForm.js
+++ modules/bookForm.js
@@ -1,5 +1,23 @@
-export default class BookForm {
-  constructor(name) {
-    this.form = document.forms[name];
+import Book from './Book.js';
+import { library } from './Library.js';
+
+export class BookForm {
+  constructor() {
+    this.form = document.forms.bookForm;
+    this.form.onsubmit = this.submit;
+  }
+
+  submit = (e) => {
+    e.preventDefault();
+    const title = this.form.title.value;
+    const author = this.form.author.value;
+    const newBook = new Book(title, author);
+
+    library.addBook(newBook);
+
+    this.form.title.focus();
+    this.form.reset();
   }
 }
+
+export default new BookForm();
